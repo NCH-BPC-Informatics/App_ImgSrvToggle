1b29d4788c9f7e261c664ed4cc87ffe7f3a4b143
Phillip Johnson <phillip@letstalkdata.com>
2020-04-29 08:26:15 -0400

    Uninstall service if already exists

--- ServiceInstaller.ps1
+++ ServiceInstaller.ps1
@@ -4,6 +4,15 @@ $powershellpath = (Get-Command powershell).Source
 $scriptPath = 'C:\App_ImgSrvToggle\AperioServiceFlagFileWatcher.ps1'
 $arguments = '-ExecutionPolicy Bypass -NoProfile -File "{0}"' -f $scriptPath
 
+# Remove service if it already exists
+if (Get-Service $serviceName -ErrorAction SilentlyContinue) {
+    If ((Get-Service $serviceName).Status -eq 'Running') {
+        Stop-Service $serviceName
+        Write-Host "Stopping $serviceName"
+    }
+	& $nssm remove $serviceName confirm
+} 
+
 $username = 'HOSPITAL\SA_BPCImageScanning'
 $password = Read-Host "Enter the password for $username" -AsSecureString
 $user = New-Object PSCredential $username,$password
@@ -11,6 +20,7 @@ $passwordRaw = $user.GetNetworkCredential().Password
 
 & $nssm install $serviceName $powershellpath $arguments
 & $nssm set $serviceName ObjectName $username $passwordRaw
+& $nssm set $serviceName Description 'Watches for flag files to toggle Aperio services.'
 
 & $nssm status $serviceName
 Start-Service $serviceName
